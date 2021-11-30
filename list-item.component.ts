4c490fb0135b355dc096eb15f3f78177816c5101
Катерина <89384547+KatrinSha@users.noreply.github.com>
2021-11-30 15:13:47 +0300

    add an intermediate solution

---
+++ list-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss']
+})
+export class ListItemComponent implements OnInit, OnDestroy {
+  @Input() listItem: any;
+  constructor() { }
+  ngOnInit(): void {
+    console.log("list-item ngOnInit")
+  }
+  ngOnDestroy(): void {
+    console.log("list-item OnDestroy")
+  }
+}
